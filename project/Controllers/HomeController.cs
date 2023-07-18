db8a6cdc12badc159350f80bcc33dbdad2992aa4
Alexander <107309506+snrtro@users.noreply.github.com>
2023-07-19 00:14:31 +0500

    home page

--- project/Controllers/HomeController.cs
+++ project/Controllers/HomeController.cs
@@ -1,7 +1,16 @@
-﻿namespace project.Controllers
-{
-    public class HomeController
-    {
+﻿using Microsoft.AspNetCore.Mvc;
 
+namespace project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index() 
+        {
+            return View();
+        }
     }
 }
